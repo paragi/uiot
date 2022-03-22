98e697cec8d3df32f2f926982420c3d20443328c
paragi <simon.riget@gmail.com>
2022-03-22 22:21:01 +0100

    SQL aøgning

---
+++ main.py
@@ -0,0 +1,45 @@
+import json
+import urllib.request
+import requests
+
+
+def test1():
+    # Constructing more specific searches seems to require authorisation
+    url = 'https://api.energidataservice.dk/datastore_search?resource_id=elspotprices'
+    fileobj = urllib.request.urlopen(url)
+    elspotprice_dict = json.loads(fileobj.read())
+    print(json.dumps(elspotprice_dict, indent=2))
+
+def test2():
+    query = '''
+    SELECT
+        COUNT(*) 
+            AS cnt, to_char(date_trunc('day', "HourUTC"), 'yyyy-mm-dd')
+            AS dc
+    FROM
+        "fcrreservesdk1"
+    WHERE
+        "HourUTC" > ((current_timestamp at time zone 'UTC') - INTERVAL '30 days')
+    GROUP BY
+        date_trunc('day', "HourUTC")
+    ORDER BY
+        date_trunc('day', "HourUTC")
+    '''
+    uri = "https://api.energidataservice.dk/datastore_search_sql?sql="
+    fileobj = urllib.request.urlopen(uri + requests.utils.quote(query))
+    elspotprice_dict = json.loads(fileobj.read())
+    print(json.dumps(elspotprice_dict, indent=2))
+
+def test3():
+    #ChargeOwner GLN_Number ChargeType ChargeTypeCode Note Description ValidFrom ValidTo VATClass Price1 Price2 Price3 Price4 Price5 Price6 Price7 Price8 Price9 Price10 Price11 Price12 Price13 Price14 Price15 Price16 Price17 Price18 Price19 Price20 Price21 Price22 Price23 Price24 TransparentInvoicing TaxIndicator ResolutionDuration
+
+    query = '''
+    SELECT * FROM "datahubpricelist"
+    ORDER BY "ValidFrom" desc
+    '''
+    uri = "https://api.energidataservice.dk/datastore_search_sql?sql="
+    fileobj = urllib.request.urlopen(uri + requests.utils.quote(query))
+    elspotprice_dict = json.loads(fileobj.read())
+    print(json.dumps(elspotprice_dict, indent=2))
+
+test1()
--- test1.py
+++
@@ -1,8 +0,0 @@
-import json
-import urllib.request
-
-url = 'https://api.energidataservice.dk/datastore_search?resource_id=elspotprices&limit=5'
-fileobj = urllib.request.urlopen(url)
-elspotprice_dict=json.loads(fileobj.read())
-print(json.dumps(elspotprice_dict,indent=2))
-
