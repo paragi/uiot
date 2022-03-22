7b723cad93fac55cfebe63200c596c34e5f2b585
paragi <simon.riget@gmail.com>
2022-03-22 22:35:17 +0100

    SQL søgning

--- main.py
+++ main.py
@@ -12,18 +12,13 @@ def test1():
 
 def test2():
     query = '''
-    SELECT
-        COUNT(*) 
-            AS cnt, to_char(date_trunc('day', "HourUTC"), 'yyyy-mm-dd')
-            AS dc
-    FROM
-        "fcrreservesdk1"
-    WHERE
-        "HourUTC" > ((current_timestamp at time zone 'UTC') - INTERVAL '30 days')
-    GROUP BY
-        date_trunc('day', "HourUTC")
-    ORDER BY
-        date_trunc('day', "HourUTC")
+    SELECT COUNT(*) 
+        AS cnt, to_char(date_trunc('day', "HourUTC"), 'yyyy-mm-dd')
+        AS dc
+    FROM "fcrreservesdk1"
+    WHERE "HourUTC" > ((current_timestamp at time zone 'UTC') - INTERVAL '30 days')
+    GROUP BY date_trunc('day', "HourUTC")
+    ORDER BY date_trunc('day', "HourUTC")
     '''
     uri = "https://api.energidataservice.dk/datastore_search_sql?sql="
     fileobj = urllib.request.urlopen(uri + requests.utils.quote(query))
@@ -34,7 +29,9 @@ def test3():
     #ChargeOwner GLN_Number ChargeType ChargeTypeCode Note Description ValidFrom ValidTo VATClass Price1 Price2 Price3 Price4 Price5 Price6 Price7 Price8 Price9 Price10 Price11 Price12 Price13 Price14 Price15 Price16 Price17 Price18 Price19 Price20 Price21 Price22 Price23 Price24 TransparentInvoicing TaxIndicator ResolutionDuration
 
     query = '''
-    SELECT * FROM "datahubpricelist"
+    SELECT * 
+    FROM "datahubpricelist"
+    WHERE "ValidFrom" >= NOW()
     ORDER BY "ValidFrom" desc
     '''
     uri = "https://api.energidataservice.dk/datastore_search_sql?sql="
@@ -42,4 +39,15 @@ def test3():
     elspotprice_dict = json.loads(fileobj.read())
     print(json.dumps(elspotprice_dict, indent=2))
 
-test1()
+def charge_owners():
+    query = '''
+    SELECT DISTINCT "ChargeOwner" 
+    FROM "datahubpricelist"
+    ORDER BY "ChargeOwner"
+    '''
+    uri = "https://api.energidataservice.dk/datastore_search_sql?sql="
+    fileobj = urllib.request.urlopen(uri + requests.utils.quote(query))
+    result_dict = json.loads(fileobj.read())
+    print(json.dumps(result_dict, indent=2))
+
+charge_owners()
